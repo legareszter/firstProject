9933b5abaef9bb79ab056a0909f4245999028129
legareszter <legareszter@gmail.com>
2011-01-06 22:53:15 +0100

    végre műkszik

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.3'
+gem 'sqlite3-ruby', :require => 'sqlite3'
+gem 'gravatar_image_tag'
+gem 'will_paginate', '3.0.pre2'
+
+group :development, :test do
+  gem 'heroku'
+  gem 'taps'
+  gem 'annotate-models', '1.0.4'
+  gem 'faker', '0.3.1'
+  gem 'factory_girl_rails', '1.0'
+  gem 'rspec', '2.3.0'
+  gem 'rspec-rails', '2.3.0'
+  gem 'webrat', '0.7.1'
+#  gem 'autotest', '4.4.6'
+#  gem 'autotest-rails-pure', '4.1.2'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railstut::Application.config.secret_token = '12bb840a93ada5d238fb2faffb222b7a400204f17d52929af45b6b2e1432b6886b7d95649b35cc1d528c82b3da97c51053331407ad823159aa348b75e5b31e52'
